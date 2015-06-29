1f2d9ad8fa8083ad982f84abd597d5620d9fe27f
anonymoussc <50c5ac69@opayq.com>
2015-06-29 11:32:50 +0700

    asdfasdfasdfasdfasdfasdfadsf

---
+++ atcc01/actr03/test/protractor.conf.js
@@ -0,0 +1,11 @@
+/**
+ * Created by anonymoussc on 6/29/15 11:09 AM.
+ */
+
+exports.config = {
+    seleniumAddress : 'http://localhost:4444/wd/hub',
+    jasmineNodeOpts : {
+        showColors             : true,
+        defaultTimeoutInterval : 30000
+    }
+};
